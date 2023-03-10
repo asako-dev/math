908d7c43bade608034183ff03f0ca2a3dda2ac2e
asako <asako0128@gmail.com>
2023-03-10 12:25:22 +0900

    スタイル変更

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const button = document.querySelector('#button');
+const btn = document.querySelector('#button');
 const random = document.querySelector('#random');
+const sum = document.querySelector('#sum');
 
-button.addEventListener('click', () => {
+btn.addEventListener('click', () => {
     const randomNumber = Math.floor(Math.random() * 100);
     if (randomNumber < 10) {
         randomNumber = '0' + randomNumber;
     }
     random.textContent = randomNumber;
+    // btn.style.opacity= 0.8;
 });
